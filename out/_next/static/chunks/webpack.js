babc3322991cb28c156ec4954b0ce80c9483bdaf
Dominik Feger <dominik.feger@abf.ch>
2020-10-29 16:53:30 +0100

    build 0.1.0
    Co-Authored-By: Dominik 'Lawlez' Feger <8553841+lawlez@users.noreply.github.com>

--- out/_next/static/chunks/webpack.js
+++ out/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0304c80d1c5e24f61f02";
+/******/ 	var hotCurrentHash = "e4636645379e326d5385";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ out/_next/static/kGX4j98kvF7RZCulOeYaw/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Findex.old","\u002Fblog\u002F[blog]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
