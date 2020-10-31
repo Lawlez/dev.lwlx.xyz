e94c1eb5eb5ae9719b44b870090af557343c3497
Dominik Feger <dominik.feger@abf.ch>
2020-10-31 16:16:11 +0100

    build 0.3.0
    Co-Authored-By: Dominik 'Lawlez' Feger <8553841+lawlez@users.noreply.github.com>

--- out/_next/static/chunks/webpack.js
+++ out/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d021730b5a493e570c92";
+/******/ 	var hotCurrentHash = "8af63320706272af5c95";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
