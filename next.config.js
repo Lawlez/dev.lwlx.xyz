da805492753279d4c3834ac944e921978e913a1d
Dominik 'Lawlez' Feger <dk.feger@gmail.com>
2020-11-01 19:44:33 +0100

    adds BundleAnalyzerPlugin to webpack

--- next.config.js
+++ next.config.js
@@ -1,14 +1,23 @@
+const BundleAnalyzerPlugin = require('@bundle-analyzer/webpack-plugin')
+
 module.exports = {
   trailingSlash: true,
   webpack: function (config) {
+    
     config.module.rules.push({
       test: /\.md$/,
       use: 'raw-loader',
-    });
+    })
+    
     config.resolve.alias = {
       ...config.resolve.alias,
       '~': __dirname,
-    };
-    return config;
+    }
+    
+    config.plugins.push(
+      new BundleAnalyzerPlugin({ token: process.env.ANALYZER_TOKEN })
+    )
+    
+    return config
   }
-};
+}
