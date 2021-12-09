36d5a2e5eba7cc8c69ff435fb9bdad7f425bc6aa
Dominik Feger <dk.feger@gmail.com>
2021-12-09 04:24:05 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,149 +1,155 @@
-import Head from "next/head";
-import dynamic from "next/dynamic";
-import { PostData, loadBlogPosts } from "../loader";
+import Head from 'next/head'
+import dynamic from 'next/dynamic'
+import { PostData, loadBlogPosts } from '../loader'
 //import { generateRSS } from "../rssUtil";
-import { globals } from "../globals";
+import { globals } from '../globals'
+import { Box } from '@mui/system'
 
-const MaterialPostCard = dynamic(import("@components/MaterialPostCard"));
-
-const sectionStyle = {
-  width: "100%",
-  padding: "100px 3vw",
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-} as const;
+const MaterialPostCard = dynamic(import('@components/MaterialPostCard'))
 
 const Home = (props: { introduction: string; posts: PostData[] }) => {
   return (
-    <div
-      style={{
-        width: "100%",
-      }}
-    >
+    <Box width='100%'>
       <Head>
         <title>dev.lwlx.xyz | Cybersecurity & Developer blog</title>
         <meta
-          name="description"
-          content="dev.lwlx.xyz publishes articles and research about Cybersecurity, DevOps, Development or just general IT nonsense. "
+          name='description'
+          content='dev.lwlx.xyz publishes articles and research about Cybersecurity, DevOps, Development or just general IT nonsense. '
         />
         <meta
-          property="og:image"
+          property='og:image'
           content={`https://dev.lwlx.xyz/lwlzcolors-lawlez-sm.jpg`}
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <div style={sectionStyle}>
+      <Box
+        width='100%'
+        padding='100px 3vw'
+        display='flex'
+        flexDirection='column'
+        alignItems='center'
+      >
         <h2
           style={{
-            margin: "4px 0px",
-            fontSize: "32pt",
+            margin: '4px 0px',
+            fontSize: '32pt',
           }}
         >
           newest posts
         </h2>
-        <div
+        <Box
           style={{
-            display: "grid",
+            display: 'grid',
             gridTemplateColumns: `repeat(auto-fit, minmax(300px, 1fr))`,
-            gridRowGap: "8px",
-            gridColumnGap: "8px",
-            width: "100%",
-            padding: "0px 7vw",
+            gridRowGap: '8px',
+            gridColumnGap: '8px',
+            width: '100%',
+            padding: '0px 7vw',
           }}
         >
           {props.posts.map((post, key) => {
             if (post && post.tags) {
               if (key === 0 || key === 1 || key === 2 || key === 3) {
-                return <MaterialPostCard key={key} post={post} />;
+                return <MaterialPostCard key={key} post={post} />
               }
-              return;
+              return
             }
-            return `the post ${post.title} has no tags defined`;
+            return `the post ${post.title} has no tags defined`
           })}
-        </div>
-      </div>
+        </Box>
+      </Box>
 
-      <div style={sectionStyle}>
+      <Box
+        width='100%'
+        padding='100px 3vw'
+        display='flex'
+        flexDirection='column'
+        alignItems='center'
+      >
         <h2
           style={{
-            margin: "4px 0px",
-            fontSize: "32pt",
+            margin: '4px 0px',
+            fontSize: '32pt',
           }}
         >
           All Posts
         </h2>
-        <div
+        <Box
           style={{
-            display: "grid",
+            display: 'grid',
             gridTemplateColumns: `repeat(auto-fit, minmax(300px, 1fr))`,
-            gridRowGap: "8px",
-            gridColumnGap: "8px",
-            width: "100%",
-            padding: "0px 7vw",
+            gridRowGap: '8px',
+            gridColumnGap: '8px',
+            width: '100%',
+            padding: '0px 7vw',
           }}
         >
           {props.posts.map((post, key) => {
-            return <MaterialPostCard key={key} post={post} />;
+            return <MaterialPostCard key={key} post={post} />
           })}
-        </div>
-      </div>
+        </Box>
+      </Box>
 
-      <div style={{ ...sectionStyle }}>
+      <Box
+        width='100%'
+        padding='100px 3vw'
+        display='flex'
+        flexDirection='column'
+        alignItems='center'
+      >
         <h2
           style={{
-            textAlign: "center",
-            fontSize: "34pt",
+            textAlign: 'center',
+            fontSize: '34pt',
           }}
         >
           Open Source Security & Dev Blog
         </h2>
         <p
           style={{
-            textAlign: "center",
-            maxWidth: "960px",
-            margin: "auto",
+            textAlign: 'center',
+            maxWidth: '960px',
+            margin: 'auto',
             lineHeight: 1.7,
           }}
         >
-          dev.lwlx.xyz is a blog focused on Cybersecurity and the occasional
-          code project.
+          dev.lwlx.xyz is a blog focused on Cybersecurity and the occasional code project.
         </p>
         <br />
-        <a href="https://twitter.com/0x0000005" style={{ cursor: "pointer" }}>
+        <a href='https://twitter.com/0x0000005' style={{ cursor: 'pointer' }}>
           <button
             style={{
-              padding: "10px 30px",
+              padding: '10px 30px',
               backgroundColor: globals.secondaryColor,
-              color: "white",
-              fontSize: "14pt",
-              border: "none",
-              borderRadius: "8px",
-              cursor: "pointer",
+              color: 'white',
+              fontSize: '14pt',
+              border: 'none',
+              borderRadius: '8px',
+              cursor: 'pointer',
             }}
           >
             @0x0000005 on twitter
           </button>
         </a>
-      </div>
-    </div>
-  );
-};
+      </Box>
+    </Box>
+  )
+}
 
-export default Home;
+export default Home
 
 export const getStaticProps = async () => {
   //const introduction = await loadMarkdownFile('introduction.md');
-  const posts = await loadBlogPosts();
+  const posts = await loadBlogPosts()
 
   // comment out to turn off RSS generation during build step.
   //await generateRSS(posts);
 
   const props = {
-    introduction: "empty for now :)", //introduction.contents,
+    introduction: 'empty for now :)', //introduction.contents,
     posts,
-  };
+  }
 
-  return { props };
-};
+  return { props }
+}
