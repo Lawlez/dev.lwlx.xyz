b1c6a99e17a6736b02569ff4071a16c6c95ec1e8
Dominik Feger <dk.feger@gmail.com>
2026-04-26 18:35:55 +0200

    fix exports

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from 'next'
 import { globals } from '../globals'
 
+export const dynamic = 'force-static'
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,6 +2,8 @@ import { MetadataRoute } from 'next'
 import { loadBlogPosts } from '../loader'
 import { globals } from '../globals'
 
+export const dynamic = 'force-static'
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const posts = loadBlogPosts()
 
